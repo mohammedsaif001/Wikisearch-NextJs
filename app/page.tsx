9b8b682423da20b8990e0453f8697d73a7013e6a
mohammedsaif-openturf <mohammed.saif@openturf.in>
2023-07-22 12:08:45 +0530

    Adding msg

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,13 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main className={inter.className}></main>;
+  return (
+    <main className={inter.className}>
+      <div className="flex justify-center items-center h-80">
+        <h2 className="text-2xl font-extrabold text-slate-700 mx-4">
+          Start Typing on Search Box to get Get Results from Wikipedia
+        </h2>
+      </div>
+    </main>
+  );
 }
